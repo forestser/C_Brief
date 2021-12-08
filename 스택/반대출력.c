224e37fcd4683bb5d1fcf2031b672745d548f80e
forestser <forester00@gmail.com>
2021-12-08 15:03:33 +0900

    스택 거꾸로 출력하기. 코드업 1402

---
+++ 스택/반대출력.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+ 
+int main(){
+    
+    int num;
+    int buf[1000] = {0, };
+    scanf("%d",&num);
+    
+    for(int i =0; i<num; i++){
+        scanf("%d", &buf[i]);
+    }
+  
+    for(int j = num-1; j >= 0; j--){
+        printf("%d ",buf[j]);
+    }
+ 
+    
+    return 0;
+}
+ 
